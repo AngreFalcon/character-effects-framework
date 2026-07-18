2744ae75374b378e83ceb1bb703d1fd5a0a60342
Sorre <angrepenguin@gmail.com>
2026-07-17 23:57:02 -0400

    implemented several new conditions

--- scripts/caf/npc.lua
+++ scripts/caf/npc.lua
@@ -39,10 +39,52 @@ local DYNAMIC_STATS = {
    ["magicka"] = types.Actor.stats.dynamic.magicka,
 }
 
+local ATTRIBUTES = {
+   ["agility"] = types.Actor.stats.attributes.agility,
+   ["endurance"] = types.Actor.stats.attributes.endurance,
+   ["intelligence"] = types.Actor.stats.attributes.intelligence,
+   ["luck"] = types.Actor.stats.attributes.luck,
+   ["personality"] = types.Actor.stats.attributes.personality,
+   ["speed"] = types.Actor.stats.attributes.speed,
+   ["strength"] = types.Actor.stats.attributes.strength,
+   ["willpower"] = types.Actor.stats.attributes.willpower,
+}
+
+local SKILLS = {
+   ["acrobatics"] = types.NPC.stats.skills.acrobatics,
+   ["alchemy"] = types.NPC.stats.skills.alchemy,
+   ["alteration"] = types.NPC.stats.skills.alteration,
+   ["armorer"] = types.NPC.stats.skills.armorer,
+   ["athletics"] = types.NPC.stats.skills.athletics,
+   ["axe"] = types.NPC.stats.skills.axe,
+   ["block"] = types.NPC.stats.skills.block,
+   ["bluntweapon"] = types.NPC.stats.skills.bluntweapon,
+   ["conjuration"] = types.NPC.stats.skills.conjuration,
+   ["destruction"] = types.NPC.stats.skills.destruction,
+   ["enchant"] = types.NPC.stats.skills.enchant,
+   ["handtohand"] = types.NPC.stats.skills.handtohand,
+   ["heavyarmor"] = types.NPC.stats.skills.heavyarmor,
+   ["illusion"] = types.NPC.stats.skills.illusion,
+   ["lightarmor"] = types.NPC.stats.skills.lightarmor,
+   ["longblade"] = types.NPC.stats.skills.longblade,
+   ["marksman"] = types.NPC.stats.skills.marksman,
+   ["mediumarmor"] = types.NPC.stats.skills.mediumarmor,
+   ["mercantile"] = types.NPC.stats.skills.mercantile,
+   ["mysticism"] = types.NPC.stats.skills.mysticism,
+   ["restoration"] = types.NPC.stats.skills.restoration,
+   ["security"] = types.NPC.stats.skills.security,
+   ["shortblade"] = types.NPC.stats.skills.shortblade,
+   ["sneak"] = types.NPC.stats.skills.sneak,
+   ["spear"] = types.NPC.stats.skills.spear,
+   ["speechcraft"] = types.NPC.stats.skills.speechcraft,
+   ["unarmored"] = types.NPC.stats.skills.unarmored,
+}
+
 local argonianTaper = {
    conditions = {
       {
          race = { "argonian" },
+         level = { min = 0, max = 100 },
          isMale = true,
          werewolf = false,
          dead = false,
@@ -55,10 +97,20 @@ local argonianTaper = {
             health = { min = 0.5, max = 1, percent = true },
 
          },
+         attributes = {
+            speed = { min = 0, max = 100 },
+         },
+         skills = {
+            acrobatics = { min = 0, max = 100 },
+         },
          equipmentslot = {
-            ["Pants"] = false,
-            ["Robe"] = false,
-            ["Skirt"] = false,
+            ["pants"] = false,
+            ["robe"] = false,
+            ["skirt"] = false,
+         },
+         classes = {
+            ["thief"] = false,
+            ["assassin"] = true,
          },
       },
    },
@@ -96,10 +148,41 @@ local function removeCosmetic(effectID)
    anim.removeVfx(this, effectID)
 end
 
-local function checkEquipmentSlots(equipSlots)
-   for k, v in pairs(equipSlots) do
-      local equipped = types.Actor.getEquipment(this.object, EQUIP_SLOTS[string.lower(k)])
-      if (equipped == nil and v == true) or (equipped ~= nil and v == false) then
+local function checkRace(condRace)
+   local actorRace = types.NPC.record(this.object).race
+   for i = 1, #condRace do
+      if actorRace == condRace[i] then
+         return true
+      end
+   end
+   return false
+end
+
+local function checkLevel(level)
+   local actorLevel = types.Actor.stats.level(this.object)
+   return compareRange(actorLevel.current, level) == false
+end
+
+local function checkSex(condSex)
+   return types.NPC.record(this.object).isMale == condSex
+end
+
+local function checkWerewolf(condWerewolf)
+   return types.NPC.isWerewolf(this.object) == condWerewolf
+end
+
+local function checkDead(condDead)
+   return types.Actor.isDead(this.object) == condDead
+end
+
+local function checkMWVars(mwvars)
+   local varTable = storage.globalSection(this.object.id)
+   if varTable == nil then
+      return false
+   end
+   for k, range in pairs(mwvars) do
+      local value = varTable:get(k)
+      if value == nil or compareRange(value, range, range.maxValue) == false then
          return false
       end
    end
@@ -117,40 +200,44 @@ local function checkDynStats(dynStats)
    return true
 end
 
-local function checkMWVars(mwvars)
-   local varTable = storage.globalSection(this.object.id)
-   if varTable == nil then
-      return false
-   end
-   for k, range in pairs(mwvars) do
-      local value = varTable:get(k)
-      if value == nil or compareRange(value, range, range.maxValue) == false then
+local function checkAttributes(attributes)
+   for k, range in pairs(attributes) do
+      local getAttr = ATTRIBUTES[k]
+      local attr = getAttr and getAttr(this.object)
+      if attr and compareRange(attr.modified, range, attr.base + attr.modifier) == false then
          return false
       end
    end
    return true
 end
 
-local function checkDead(condDead)
-   return types.Actor.isDead(this.object) == condDead
-end
-
-local function checkWerewolf(condWerewolf)
-   return types.NPC.isWerewolf(this.object) == condWerewolf
-end
-
-local function checkSex(condSex)
-   return types.NPC.record(this.object).isMale == condSex
-end
-
-local function checkRace(condRace)
-   local actorRace = types.NPC.record(this.object).race
-   for i = 1, #condRace do
-      if actorRace == condRace[i] then
-         return true
+local function checkSkills(skills)
+   for k, range in pairs(skills) do
+      local getSkill = SKILLS[k]
+      local skill = getSkill and getSkill(this.object)
+      if skill and compareRange(skill.modified, range, skill.base + skill.modifier) == false then
+         return false
       end
    end
-   return false
+   return true
+end
+
+local function checkEquipmentSlots(equipSlots)
+   for k, v in pairs(equipSlots) do
+      local equipped = types.Actor.getEquipment(this.object, EQUIP_SLOTS[string.lower(k)])
+      if (equipped == nil and v == true) or (equipped ~= nil and v == false) then
+         return false
+      end
+   end
+   return true
+end
+
+local function checkClass(classes)
+   local class = types.NPC.record(this.object).class
+   if (classes[string.lower(class)] == false) then
+      return false
+   end
+   return true
 end
 
 local function checkEffectConditions()
@@ -159,6 +246,10 @@ local function checkEffectConditions()
 
    for i = 1, #conditions do
       if conditions[i].race ~= nil and checkRace(conditions[i].race) == false then
+         removeCosmetic(effectID)
+         return
+      end
+      if conditions[i].level ~= nil and checkLevel(conditions[i].level) == false then
          removeCosmetic(effectID)
          return
       end
@@ -182,9 +273,19 @@ local function checkEffectConditions()
          removeCosmetic(effectID)
          return
       end
+      if conditions[i].attributes ~= nil and checkAttributes(conditions[i].attributes) == false then
+         removeCosmetic(effectID)
+         return
+      end
+      if conditions[i].skills ~= nil and checkSkills(conditions[i].skills) == false then
+         removeCosmetic(effectID)
+         return
+      end
       if conditions[i].equipmentslot ~= nil and checkEquipmentSlots(conditions[i].equipmentslot) == false then
-
-
+         removeCosmetic(effectID)
+         return
+      end
+      if conditions[i].classes ~= nil and checkClass(conditions[i].classes) == false then
          removeCosmetic(effectID)
          return
       end
